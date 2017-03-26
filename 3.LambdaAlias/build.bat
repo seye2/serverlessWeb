36fe27f7daef4265d3774826ae7c8fcd4a500e24
dh <seye2@nate.com>
2017-03-26 19:35:33 +0900

    hello world

---
+++ 3.LambdaAlias/build.bat
@@ -0,0 +1,11 @@
+del /s /q c:\project\serverlessWeb\2.Lambda\functions\world\*
+rmdir /s /q c:\project\serverlessWeb\2.Lambda\functions\world\node_modules
+
+copy c:\project\serverlessWeb\2.Lambda\app.js c:\project\serverlessWeb\2.Lambda\functions\world\app.js
+copy c:\project\serverlessWeb\2.Lambda\index.js c:\project\serverlessWeb\2.Lambda\functions\world\index.js
+copy c:\project\serverlessWeb\2.Lambda\package.json c:\project\serverlessWeb\2.Lambda\functions\world\package.json
+
+cd c:\project\serverlessWeb\2.Lambda\functions\world
+yarn && cd c:\project\serverlessWeb\2.Lambda
+
+
---
+++ 3.LambdaAlias/index.js
@@ -0,0 +1,8 @@
+// lambda.js'
+'use strict'
+
+const awsServerlessExpress = require('aws-serverless-express');
+const app = require('./app');
+const server = awsServerlessExpress.createServer(app);
+
+exports.handle = (event, context) => awsServerlessExpress.proxy(server, event, context);
